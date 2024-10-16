07ec43dcb72492d0fd3dbee1e74eed5811c34c04
Johnny Herrera <jmjaramillo95@gmail.com>
2024-10-15 23:48:24 -0500

    fetch-learn

---
+++ fetch/src/App.jsx
@@ -0,0 +1,19 @@
+import { useFetch } from './useFetch'
+import './App.css'
+
+function App() {
+  
+  const {data, loading} = useFetch("https://jsonplaceholder.typicode.com/users")
+
+  return (
+    <>
+    <h1>Fetch like a pro</h1>
+    <ul>
+      {loading && <li>Loading...</li>}
+      {data?.map((user) => (<li key={user.id}>{user.name}</li>))}
+    </ul>
+    </>
+  )
+}
+
+export default App
