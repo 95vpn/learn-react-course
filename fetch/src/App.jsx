e3fb71bbdb38ebafc819f3b64e2258323ccfff89
Johnny Herrera <jmjaramillo95@gmail.com>
2024-10-16 23:22:20 -0500

    4 formas de hacer fetch

--- fetch/src/App.jsx
+++ fetch/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 function App() {
   
-  const {data, loading} = useFetch("https://jsonplaceholder.typicode.com/users")
+  const {data, loading, error, handleCancelRequest} = useFetch("https://jsonplaceholder.typicode.com/users")
 
   return (
     <>
     <h1>Fetch like a pro</h1>
+    <button onClick={handleCancelRequest}>Cancel Request</button>
     <ul>
+      {error && <li>Error: {error}</li>}
       {loading && <li>Loading...</li>}
       {data?.map((user) => (<li key={user.id}>{user.name}</li>))}
     </ul>
--- fetch/src/useFetch.js
+++ fetch/src/useFetch.js
@@ -4,15 +4,37 @@ import { useEffect, useState } from "react"
 export function useFetch(url) {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        setLoading(true)
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => console.log(setData(data)))
-            .finally(() => setLoading(false))
-}, [])
+    const [error, setError] = useState(null)
+    const [controller, setController] = useState(null)
 
-return { data, loading }
+    useEffect(() => {
+        const abortController = new AbortController();
+        setController(abortController)
+        setLoading(true)
+        fetch(url, {signal: abortController.signal})
+            .then((response) => response.json())
+            .then((data) => setData(data))
+            .catch(error => {
+                if (error.name === "AbortError") {
+                    console.log("Request cancelled")
+                } else {
+
+                    setError(error)
+                }
+            })
+            .finally(() => setLoading(false))
+
+        return () => abortController.abort();
+}, []);
+
+const handleCancelRequest = () => {
+    if (controller) {
+        controller.abort()
+        setError("Request Cancel")
+    }
+}
+
+return { data, loading, error, handleCancelRequest }
 
 }
 
